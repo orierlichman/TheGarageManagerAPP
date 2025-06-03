8ae5a3a0e580fe0faae2ff00f69601b17937add0
User <User@LAB3-3>
2025-06-03 12:36:45 +0300

    add user

--- TheGarageManagerAPP/Services/TheGarageManagerWebAPIProxy.cs
+++ TheGarageManagerAPP/Services/TheGarageManagerWebAPIProxy.cs
@@ -14,29 +14,29 @@ namespace TheGarageManagerApp.Services
     public class TheGarageManagerWebAPIProxy
     {
 
-        #region without tunnel
+        //#region without tunnel
 
-        //Define the serevr IP address! (should be realIP address if you are using a device that is not running on the same machine as the server)
-        private static string serverIP = "localhost";
-        private HttpClient client;
-        private string baseUrl;
-        public static string BaseAddress = (DeviceInfo.Platform == DevicePlatform.Android &&
-            DeviceInfo.DeviceType == DeviceType.Virtual) ? "http://10.0.2.2:5055/api/" : $"http://{serverIP}:5055/api/";
-        private static string ImageBaseAddress = (DeviceInfo.Platform == DevicePlatform.Android &&
-            DeviceInfo.DeviceType == DeviceType.Virtual) ? "http://10.0.2.2:5055" : $"http://{serverIP}:5055";
-
-        #endregion
-
-        //#region with tunnel
         ////Define the serevr IP address! (should be realIP address if you are using a device that is not running on the same machine as the server)
-
-        //private static string serverIP = "l4dfcs7m-5055.euw.devtunnels.ms";
+        //private static string serverIP = "localhost";
         //private HttpClient client;
         //private string baseUrl;
-        //public static string BaseAddress = "https://l4dfcs7m-5055.euw.devtunnels.ms/api/";
-        //private static string ImageBaseAddress = "https://l4dfcs7m-5055.euw.devtunnels.ms/";
-       
+        //public static string BaseAddress = (DeviceInfo.Platform == DevicePlatform.Android &&
+        //    DeviceInfo.DeviceType == DeviceType.Virtual) ? "http://10.0.2.2:5055/api/" : $"http://{serverIP}:5055/api/";
+        //private static string ImageBaseAddress = (DeviceInfo.Platform == DevicePlatform.Android &&
+        //    DeviceInfo.DeviceType == DeviceType.Virtual) ? "http://10.0.2.2:5055" : $"http://{serverIP}:5055";
+
         //#endregion
+
+        #region with tunnel
+        //Define the serevr IP address! (should be realIP address if you are using a device that is not running on the same machine as the server)
+
+        private static string serverIP = "l4dfcs7m-5055.euw.devtunnels.ms";
+        private HttpClient client;
+        private string baseUrl;
+        public static string BaseAddress = "https://l4dfcs7m-5055.euw.devtunnels.ms/api/";
+        private static string ImageBaseAddress = "https://l4dfcs7m-5055.euw.devtunnels.ms/";
+
+        #endregion
 
         public TheGarageManagerWebAPIProxy()
         {
